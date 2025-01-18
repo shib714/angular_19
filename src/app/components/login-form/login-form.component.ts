4e64e41172f9ff3fd4299ac6caff8f6f9c6a8e46
shib7 <shib714@gmail.com>
2025-01-17 23:19:09 -0500

    validation on the template

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule, AbstractControl } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import Validation from '../utils/validation';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { JsonPipe } from '@angular/common';
 
 
 @Component({
@@ -11,7 +14,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     ReactiveFormsModule,
     MatInputModule,
     MatButtonModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatCheckboxModule,
+    JsonPipe
   ],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss'
@@ -19,21 +24,63 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 export class LoginFormComponent {
 
   myForm!: FormGroup;
+  submitted = false;
+  fb = inject(FormBuilder);
 
-  constructor(private fb: FormBuilder) {}
+  //constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.myForm = this.fb.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
-    });
+    this.createForm()    
+    console.log(this.myForm.controls);
+    console.log(this.myForm.controls['fullname'].value);
   }
 
+  createForm() : void {
+    this.myForm = this.fb.group(
+      {
+        fullname: ['', Validators.required],
+        //firstName: this.fb.control<string>('', Validators.required),
+        username: ['',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(20)
+          ]
+        ],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(40)
+          ]
+        ],
+        confirmPassword: ['', Validators.required],
+        acceptTerms: [false, Validators.requiredTrue]
+      },
+      {
+        validators: [Validation.match('password', 'confirmPassword')]
+      }
+    );
+  }
+
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.myForm.controls;
+  }
   onSubmit(): void {
+    this.submitted = true;
+    // if (this.myForm.invalid) {
+    //   return;
+    // } 
     if (this.myForm.valid) {
       console.log(this.myForm.value);
+      console.log(JSON.stringify(this.myForm.value, null, 2));
     }
+  }
+  onReset(): void {
+    this.submitted = false;
+    this.myForm.reset();
   }
 
 }
