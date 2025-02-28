ec437e7b8ea70f7b175e7f022759a569e39f9411
shib7 <shib714@gmail.com>
2025-02-28 16:15:21 -0500

    use ngTemplateOutlet to render the weather widget

--- src/app/components/ng-template/weather-widget/weather-widget.component.ts
+++ src/app/components/ng-template/weather-widget/weather-widget.component.ts
@@ -1,14 +1,21 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
 import { WidgetActions } from '../widget-actions.service';
 import { WidgetState } from '../widget-state.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-weather-widget',
-  imports: [],
+  imports: [CommonModule],
   template: `
     <div class="widget-header">
-      <div class="widget-title">Weather Forecast</div>
-      <div class="widget-sub-title">Current weather in your location</div>
+         <!-- <ng-container #container></ng-container>  -->
+         <ng-container [ngTemplateOutlet]="defaultWidgetHeader"></ng-container> 
+        <ng-template #defaultWidgetHeader>
+          <div class="widget-title">Weather Forecast</div>
+          <div class="widget-sub-title">Current weather in your location</div>
+          <div class="widget-sub-title">Last updated at: {{state.lastUpdated | date: 'yyyy-mm-dd HHH-mm-ssSSS'}}</div>
+        </ng-template>
+
     </div>    
     <div class="widget-content">
       <div class="weather-icon">
@@ -16,9 +23,9 @@ import { WidgetState } from '../widget-state.service';
       </div>
       <div class="weather-data">
         <div class="temperature"></div>
-        <div class="sky-condition">Sunny</div>
-        <div class="temperature">72°F</div>
-        <div class="windspeed">10 mph</div>
+        <div class="sky-condition">{{state.data.skyCondition}}</div>
+        <div class="temperature">{{state.data.temperature}}°F</div>
+        <div class="windspeed">{{state.data.windspeed}} mph</div>
       </div>
     </div>
     <div class="widget-actions">
@@ -34,6 +41,21 @@ export class WeatherWidgetComponent {
 
   state = inject(WidgetState, { self: true });
   actions = inject(WidgetActions, { self: true });
+
+  //declerative way to create a template
+  // @ViewChild('container', {read: ViewContainerRef}) container!: ViewContainerRef;
+  // @ViewChild('defaultWidgetHeader') headerTemplate!: TemplateRef<any>;
+
+  // ngAfterViewInit() {
+  //   this.container.createEmbeddedView(this.headerTemplate);
+  // }
+
+  //we can also use NGTemplateOutlet directive (from CommonModule) to render the template alternatively
+  //we will commented out the above code and use the below code to the template to render
+  // <ng-container [ngTemplateOutlet]="defaultWidgetHeader"></ng-container> and it will work the same way
+  // resulting to write less code
+
+  //s
 
 
 
